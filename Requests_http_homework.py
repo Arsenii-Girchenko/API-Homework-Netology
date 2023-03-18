3629eaf7f903708f6d543c91953c5aacad736617
Arsenii-Girchenko <120548163+Arsenii-Girchenko@users.noreply.github.com>
2023-03-18 12:05:29 +0300

    Fixed errors in Task 2

--- Requests_http_homework.py
+++ Requests_http_homework.py
@@ -22,53 +22,49 @@ import requests
 #     pprint(f'Hero with the highest {stat} stat is {compare_heroes_by_stat(url, heroes_list, stat)}')
     
     
-# # Задача №2
+# Задача №2
 
-# class YaUploader:
-#     def __init__(self, token: str):
-#         self.token = token
-#         self.yandex_url = "https://cloud-api.yandex.net"
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
+        self.yandex_url = 'https://cloud-api.yandex.net'
         
-#     def get_headers(self):
-#         return {
-#             'Content-Type': 'application/json',
-#             'Authorization': f'OAuth {self.token}'
-#         }
-        
-#     def _get_upload_link(self, disk_file_path):
-#         upload_url = f'{self.yandex_url}/v1/disk/resources/upload'
-#         headers = self.get_headers()
-#         params = {"path": disk_file_path, "overwrite": "true"}
-#         response = requests.get(upload_url, headers=headers, params=params)
-#         return response.json()   
-
-#     def upload_file_to_disk(self, disk_file_path, filename):
-#         href_response = self._get_upload_link(disk_file_path=disk_file_path)
-#         href = href_response.get("href", "")
-#         response = requests.put(url=href, data=open(filename, 'rb'))
-#         response.raise_for_status()
-#         if response.status_code == 201:
-#             print("Success")
-
-# if __name__ == '__main__':
-#     ya = YaUploader(TOKEN)
-#     ya.upload_file_to_disk('Netology', "File_for_Yandex.txt")
-
-
-# № Задача №3
-
-from datetime import datetime, timedelta
-import time
-
-
-def stackoverflow_questions_search(search_word: str):
-    start_date = datetime.now() - timedelta(days=2)
-    stop_date = datetime.now()
-    start_date_unix = int(time.mktime(start_date.timetuple()))
-    stop_date_unix = int(time.mktime(stop_date.timetuple()))
-    url =  f'https://api.stackexchange.com/2.3/questions?fromdate={start_date_unix}&todate={stop_date_unix}&order=desc&sort=activity&tagged={search_word}&site=stackoverflow'
-    questions_info = requests.get(url=url).json()
-    pprint(questions_info)
+    def get_headers(self):
+        return {
+            'Accept': 'application/json',
+            'Authorization': f'OAuth {self.token}'
+        }
+    def get_upload_link(self, where_to_upload):
+        upload_url = f'{self.yandex_url}/v1/disk/resources/upload'
+        headers = self.get_headers()
+        params = {'path': where_to_upload, 'overwrite': 'true'}
+        response = requests.get(upload_url, params=params, headers=headers)
+        return response.json()
     
-if __name__ == '__main__':
-    stackoverflow_questions_search('python')
+    def upload_file_to_disk(self, where_to_upload, filename):
+        upload_response = self.get_upload_link(where_to_upload)
+        upload_url = upload_response.get('href')
+        response = requests.put(upload_url, files={'file': open(filename, 'rb')}, headers=self.get_headers())
+        print(response)
+        
+ya = YaUploader(TOKEN)
+ya.upload_file_to_disk('/Uploaded_file.txt', 'File_for_Yandex.txt')
+
+
+# # № Задача №3
+
+# from datetime import datetime, timedelta
+# import time
+
+
+# def stackoverflow_questions_search(search_word: str):
+#     start_date = datetime.now() - timedelta(days=2)
+#     stop_date = datetime.now()
+#     start_date_unix = int(time.mktime(start_date.timetuple()))
+#     stop_date_unix = int(time.mktime(stop_date.timetuple()))
+#     url =  f'https://api.stackexchange.com/2.3/questions?fromdate={start_date_unix}&todate={stop_date_unix}&order=desc&sort=activity&tagged={search_word}&site=stackoverflow'
+#     questions_info = requests.get(url=url).json()
+#     pprint(questions_info)
+    
+# if __name__ == '__main__':
+#     stackoverflow_questions_search('python')
